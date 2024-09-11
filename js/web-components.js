d05858ab023523db614251da999d7cf5bf752280
shemsonex <shemsonex@gmail.com>
2024-09-11 17:04:26 +0200

    Added Book Table section

--- js/web-components.js
+++ js/web-components.js
@@ -91,17 +91,87 @@ window.customElements.define('hero-section', HeroSection);
 
 
 const template_booktable = document.createElement('template');
-template_booktable.innerHTML = `<section class="hero-wrap hero-wrap-2" style="background-image: url('images/4j.jpeg');" data-stellar-background-ratio="0.5">
-		<div class="overlay"></div>
-		<div class="container">
-			<div class="row no-gutters slider-text align-items-end justify-content-center">
-				<div class="col-md-9 ftco-animate text-center mb-5">
-					<h1 class="mb-2 bread">Book A Table Now</h1>
-					<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>Reservation <i class="fa fa-chevron-right"></i></span></p>
-				</div>
-			</div>
-		</div>
-	</section>`;
+template_booktable.innerHTML = `<section class="ftco-section ftco-wrap-about ftco-no-pb ftco-no-pt">
+<div class="container">
+  <div class="row no-gutters">
+    <div class="col-sm-4 p-4 p-md-5 d-flex align-items-center justify-content-center bg-primary">
+      <form action="#" class="appointment-form">
+        <h3 class="mb-3">Book your Table</h3>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <input type="name" class="form-control" placeholder="Name">
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="form-group">
+              <input type="email" class="form-control" placeholder="Email">
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Phone">
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="form-group">
+              <div class="input-wrap">
+                <div class="icon"><span class="fa fa-calendar"></span></div>
+                <input type="text" class="form-control book_date" placeholder="Check-In">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="form-group">
+              <div class="input-wrap">
+                <div class="icon"><span class="fa fa-clock-o"></span></div>
+                <input type="text" class="form-control book_time" placeholder="Time">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="form-group">
+              <div class="form-field">
+                <div class="select-wrap">
+                  <div class="icon"><span class="fa fa-chevron-down"></span></div>
+                  <select name="" id="" class="form-control">
+                    <option value="">Guest</option>
+                    <option value="">1</option>
+                    <option value="">2</option>
+                    <option value="">3</option>
+                    <option value="">4</option>
+                    <option value="">5</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="form-group">
+              <input type="submit" value="Book Your Table Now" class="btn btn-white py-3 px-4">
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-sm-8 wrap-about py-5 ftco-animate img" style="background-image: url(images/1h.jpg);">
+      <div class="row pb-5 pb-md-0">
+        <div class="col-md-12 col-lg-7">
+          <div class="heading-section mt-5 mb-4">
+            <div class="pl-lg-3 ml-md-5">
+              <span class="subheading"></span>
+              <h2 class="mb-4">Welcome to BRACHETTO</h2>
+            </div>
+          </div>
+          <div class="pl-lg-3 ml-md-5">
+            <p> 								</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>`;
     
 class BookTableSection extends HTMLElement {
   constructor() {
